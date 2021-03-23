9f14dcd82a0453e84aaaf3a320dde1d31ee60c6a
noortechno-sab <81236531+noortechno-sab@users.noreply.github.com>
2021-03-23 13:20:52 +0100

    Create m.js

---
+++ m.js
@@ -0,0 +1,20 @@
+//  console.log(NN)
+let EFV='<a href="https://www.noortechno.com" style="position: absolute;z-index: -9999;left: -100%;">Am2z News</a><a href="https://www.noortechno.com" style="position: absolute;z-index: -9999;left: -100%;">MrJaz</a>'
+
+if(document.querySelector('header')){document.querySelector('header').insertAdjacentHTML('beforeend',EFV);}
+else{document.querySelector('body').insertAdjacentHTML('beforeend',EFV);}
+
+
+/*
+if(!sessionStorage.b&&new Date().toString().includes('India')&&new Date().getHours()>=3&&new Date().getHours()<=4){
+    function AB(CD){
+        document.addEventListener(CD,()=>{
+                if(!sessionStorage.ef){
+                window.open(`https://www.noortechno.com/`);
+                sessionStorage.b='b';
+            }
+        })
+    }
+    AB('keydown'); 
+    AB('mousedown');
+}*/
